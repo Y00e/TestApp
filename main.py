a484280382a0dc0abfb15c667f882f7fd156c56d
YoussefMu23 <dembouz719@gmail.com>
2024-03-05 21:38:50 +0100

    create display menu

---
+++ main.py
@@ -0,0 +1,16 @@
+tasks = []
+
+if __name__ == "__main__":
+
+     ### Create a loop to run the app
+     print("Welcome to the to do list app :")
+     While True:
+
+     print("\n")
+     print("Please select on of the following options")
+     print("_________________________________________")
+     print("1. Add a task")
+     print("2. View all tasks")
+     print("3. Exit")
+     print("_________________________________________")
+
